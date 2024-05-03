4d7773988824a87841093d78e9a38d25b4ec937b
Matt Wyman <matt@okareo.com>
2024-05-03 07:58:38 -0700

    removing global test setup that is not needed

--- jest-setup.ts
+++
@@ -1,7 +0,0 @@
-import { getProjectId } from './tests/setup-env';
-
-module.exports = async function (globalConfig: any, projectConfig: any) {
-
-    // called here to create a project if needed before the tests run.
-    const project_id = await getProjectId();
-};
--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,4 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testTimeout: 100000,
-  globalSetup: './jest-setup.ts',
 };
